a45ba01bb97a4a727d36732e26610834a73b5b51
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2025-12-19 21:14:42 +0200

    Api work. Simple UserController added to test out stuff

---
+++ FitLead/FitLead.Api/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using FitLead.Application.Users.CreateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitLead.Api.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public sealed class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.IsSuccess)
+                return BadRequest(result.Error);
+
+            return Ok(new { userId = result.Value });
+        }
+    }
+}
