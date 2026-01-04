c20a90d28b23d99ae3c1d8c39ebfcc92f71f685d
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2026-01-04 13:31:01 +0200

    Invitation controller added

---
+++ FitLead/FitLead.Api/Controllers/InvitationsController.cs
@@ -0,0 +1,44 @@
+﻿using FitLead.Application.Invitations.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitLead.Api.Controllers
+{
+    [ApiController]
+    [Route("api/invitations")]
+    public sealed class InvitationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public InvitationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("client/{clientId}")]
+        public async Task<IActionResult> GetPendingForClient(
+            Guid clientId,
+            CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(
+                new GetPendingInvitationsForClientQuery(
+                    clientId,
+                    DateTime.UtcNow),
+                cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet("trainer/{trainerId}")]
+        public async Task<IActionResult> GetSentByTrainer(
+            Guid trainerId,
+            CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(
+                new GetSentInvitationsByTrainerQuery(trainerId),
+                cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
