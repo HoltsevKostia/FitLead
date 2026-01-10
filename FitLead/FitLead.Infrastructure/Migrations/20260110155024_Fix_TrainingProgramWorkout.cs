3e5c9e686797086f4f920db62938a45856d5472f
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2026-01-10 17:52:50 +0200

    feat: add successful migrations to address config changes

---
+++ FitLead/FitLead.Infrastructure/Migrations/20260110155024_Fix_TrainingProgramWorkout.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitLead.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_TrainingProgramWorkout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_training_program_workouts_training_programs_training_progra~",
+                table: "training_program_workouts");
+
+            migrationBuilder.RenameColumn(
+                name: "training_program_id",
+                table: "training_program_workouts",
+                newName: "TrainingProgramId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_training_program_workouts_training_program_id",
+                table: "training_program_workouts",
+                newName: "IX_training_program_workouts_TrainingProgramId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_training_program_workouts_training_programs_TrainingProgram~",
+                table: "training_program_workouts",
+                column: "TrainingProgramId",
+                principalTable: "training_programs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_training_program_workouts_training_programs_TrainingProgram~",
+                table: "training_program_workouts");
+
+            migrationBuilder.RenameColumn(
+                name: "TrainingProgramId",
+                table: "training_program_workouts",
+                newName: "training_program_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_training_program_workouts_TrainingProgramId",
+                table: "training_program_workouts",
+                newName: "IX_training_program_workouts_training_program_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_training_program_workouts_training_programs_training_progra~",
+                table: "training_program_workouts",
+                column: "training_program_id",
+                principalTable: "training_programs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
