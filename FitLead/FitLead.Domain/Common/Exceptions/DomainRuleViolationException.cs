74da535b49f9f4c53bbe8cffe6c85adf73a2182c
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2026-02-21 10:55:08 +0200

    chore: cleanup Domain code

--- FitLead/FitLead.Domain/Common/Exceptions/DomainRuleViolationException.cs
+++ FitLead/FitLead.Domain/Common/Exceptions/DomainRuleViolationException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FitLead.Domain.Common.Exceptions
+﻿namespace FitLead.Domain.Common.Exceptions
 {
     public sealed class DomainRuleViolationException : DomainException
     {
--- FitLead/FitLead.Domain/Users/User.cs
+++ FitLead/FitLead.Domain/Users/User.cs
@@ -1,9 +1,4 @@
 using FitLead.Common.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FitLead.Domain.Users
 {
